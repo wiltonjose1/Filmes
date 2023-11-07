f61aa017a1ec61e347a97127f923ccd7c5e58bb7
wiltonjose1 <zefilho1030@gmail.com>
2023-11-07 01:23:25 -0300

    Modifiquei o css do MoviesGrid.css

--- filmes_lib/src/pages/home.jsx
+++ filmes_lib/src/pages/home.jsx
@@ -1,6 +1,8 @@
 import {useState, useEffect} from "react";
 import MovieCard from "../components/MovieCard";
 
+import "./MoviesGrid.css";
+
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
@@ -22,7 +24,7 @@ const Home = () => {
 
     return ( 
         <div className="container">
-         <ha className="title">Melhores filmes</ha>
+         
          <div className="movies-container">
             {topMovies.length === 0 && <p>Carregando...</p>}
             {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
